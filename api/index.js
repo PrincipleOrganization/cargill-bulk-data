eb1d2bad9e1c687c06e290fe0ba59f54f95ded9e
ShamatienkoYaroslav <shamatienko.yaroslav@gmail.com>
2016-07-23 15:41:16 +0300

    First upload

---
+++ api/index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res) {
+  res.json({
+    api_version: 1
+  });
+});
+
+router.get('/data', function(req, res) {
+  var db = global.app.mongodb;
+  db.getData(req.body, function(err, docs) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.json(docs);
+    }
+  });
+});
+
+router.post('/data', function(req, res) {
+  var db = global.app.mongodb;
+  db.addToData(req.body, function(err, rec) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.json(rec);
+    }
+  });
+});
+
+module.exports = router;
